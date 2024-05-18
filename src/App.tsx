6b0d570faa01c2c11a3c45e976ad5c88f708dd5e
sohseyedi <sohseyedi@gmail.com>
2024-05-18 14:09:09 +0330

    routing project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
