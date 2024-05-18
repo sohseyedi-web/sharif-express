81d6de951ce3eda13891923177c05532363bc00a
sohseyedi <sohseyedi@gmail.com>
2024-05-18 19:01:54 +0330

    complete profile complete

--- src/features/auth/CompleteProfile.tsx
+++ src/features/auth/CompleteProfile.tsx
@@ -1,28 +1,58 @@
-import { useState } from "react";
 import TextField from "../../ui/TextField";
+import { type FieldValues, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { completeProfile } from "../../service/authService";
+import toast from "react-hot-toast";
 
 const CompleteProfile = () => {
-  const [name, setName] = useState("");
-  const [city, setCity] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: completeProfile,
+  });
+
+  const completedProfileHandler = async (data: FieldValues) => {
+    try {
+      const { user, message } = await mutateAsync(data);
+      toast.success(message);
+      if (user.role === "OWNER") return navigate("/owner");
+      if (user.role === "FREELANCER") return navigate("/freelancer");
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
 
   return (
-    <form className="space-y-5 mt-3">
+    <form
+      className="space-y-5 mt-3"
+      onSubmit={handleSubmit(completedProfileHandler)}
+    >
       <TextField
-        label={"نام کامل "}
-        name={name}
-        value={name}
-        placeholder="نام و نام خانوادگی"
-        onChange={(e) => setName(e.target.value)}
+        label={"نام کاربری"}
+        name="name"
+        register={register}
+        validationSchema={{
+          required: "نام کاربری ضرروی است",
+        }}
+        errors={errors}
       />
       <TextField
-        label={"شهر"}
-        name={city}
-        value={city}
-        placeholder="شهر محل سکونت"
-        onChange={(e) => setCity(e.target.value)}
+        label={"شهر محل سکونت"}
+        name="city"
+        register={register}
+        validationSchema={{
+          required: " شهر شما ضرروی است",
+        }}
+        errors={errors}
       />
       <button className="mt-2 btn btn-active btn-success w-full h-[45px] text-white">
-        تکمیل اطلاعات
+        {isPending ? "لطفا صبر کنید" : "تکمیل اطلاعات"}
       </button>
     </form>
   );
