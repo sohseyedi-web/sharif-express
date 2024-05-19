299e85ec2196117c17527d57e865e806cd1e8170
sohseyedi <sohseyedi@gmail.com>
2024-05-19 10:12:16 +0330

    admin & panel routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Panel from "./pages/Panel";
+import Admin from "./pages/Admin";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/join" element={<Auth />} />
+        <Route path="/panel" element={<Panel />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
