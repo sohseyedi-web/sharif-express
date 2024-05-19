f1fc83e080d55aaa0eecb27602c9ac8e6f4539d6
sohseyedi <sohseyedi@gmail.com>
2024-05-19 19:17:10 +0330

    orderlist ui

--- src/features/panel/Layout.tsx
+++ src/features/panel/Layout.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import * as RiIcon from "react-icons/ri";
-import { orderLists } from "../../constant/orderListValue";
-import { toPersianNumbers } from "../../utils/toPersianNumbers";
+import ListOrders from "./ListOrders";
+import { toPersianNumbersWithComma } from './../../utils/toPersianNumbers';
 
 const Layout = () => {
-  const [lists, setLists] = useState(orderLists);
-
   return (
     <>
       <header className="flex mt-5 items-center justify-center gap-x-3 flex-wrap">
@@ -19,31 +16,29 @@ const Layout = () => {
         </button>
       </header>
       <main className="max-w-2xl mx-auto mt-2 lg:px-2 px-5 p-2">
-        <form>
+        <form className="space-y-4">
           <h4 className="lg:text-xl font-semibold text-lg border-b-2 pb-2 border-green-500">
             فرم ایجاد سفارش
           </h4>
-          <section className="w-full p-2 cursor-pointer h-[180px] overflow-y-auto shadow-sm mt-2 rounded-xl">
-            <div className="flex items-center justify-between flex-wrap space-y-2">
-              {lists.map((list) => (
-                <div
-                  key={list.id}
-                  className="flex items-center justify-between gap-x-2 md:w-[45%] w-full"
-                >
-                  <button className="w-6 h-6 rounded bg-green-600 text-white">
-                    +
-                  </button>
-                  <div className="flex items-center gap-x-1">
-                    <span>{toPersianNumbers(String(list.value))}</span>
-                    <span>{list.label}</span>
-                  </div>
-                  <button className="w-6 h-6 rounded bg-green-600 text-white">
-                    -
-                  </button>
-                </div>
-              ))}
-            </div>
-          </section>
+          <ListOrders />
+
+          <hr />
+          <div className="form-control my-3">
+            <label className="cursor-pointer flex  gap-x-2 items-center">
+              <input type="checkbox" className="checkbox checkbox-success" />
+              <span className="label-text font-semibold">
+                مایلم سفارش به صورت تک شویی انجام شود
+              </span>
+            </label>
+          </div>
+          <div className="flex items-center justify-between gap-x-1">
+            <button className="btn bg-green-700 text-white w-[80%] h-[45px]">
+              قیمت : {toPersianNumbersWithComma("1000000")} تومان
+            </button>
+            <span className="text-red-500 font-semibold w-[20%] text-center cursor-pointer">
+              انصراف؟
+            </span>
+          </div>
         </form>
       </main>
     </>
