ea7a109ef668d0eddca18d9e3f4c061400951e8e
sohseyedi <sohseyedi@gmail.com>
2024-05-28 09:56:59 +0330

    order lists page

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,11 +1,19 @@
-import OrderTable from '../features/orders/OrderTable'
+import { RiArrowUpDownLine } from "react-icons/ri";
+import OrderTable from "../features/orders/OrderTable";
 
 const Orders = () => {
   return (
-    <section className='max-w-7xl mx-auto'>
-        <OrderTable/>
+    <section className="max-w-7xl mx-auto mt-7 lg:px-0 px-3">
+      <header className="flex items-center justify-between">
+        <h4 className="lg:text-xl text-lg font-semibold text-green-600">
+          لیست سفارشات من
+        </h4>
+          <RiArrowUpDownLine className="bg-green-600 cursor-pointer p-1 w-7 h-7 text-white rounded-lg"/>
+      </header>
+      <hr className="dark:border-slate-700 my-2 border-slate-200" />
+      <OrderTable />
     </section>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
