847de1576c1cf900ddb26a19cb45d4d86a78bab0
sohseyedi <sohseyedi@gmail.com>
2024-06-14 18:16:57 +0330

    top header user detail page

---
+++ src/features/admin/userDetail/TopUserDetail.tsx
@@ -0,0 +1,38 @@
+import Stats from "../../../ui/Stats/Stats";
+import Stat from "../../../ui/Stats/Stat";
+import * as RiIcon from "react-icons/ri";
+import { useParams } from "react-router-dom";
+import { useGetDetailUser } from "../../../hooks/admin/useGetAdminData";
+import toLocaleDate from "../../../utils/toLocadDate";
+import { OrderType } from "../../../lib/OrderRowTypes";
+
+const TopUserDetail = () => {
+  const { phone } = useParams();
+  const { data } = useGetDetailUser(String(phone));
+
+  const { orders, user } = data || {};
+
+  const total = orders?.reduce(
+    (acc: number, cur: OrderType) => acc + cur.price,
+    0
+  );
+
+  return (
+    <Stats>
+      <Stat title="نام مشتری" value={user?.name}>
+        <RiIcon.RiUser3Line size="29" className="text-white" />
+      </Stat>
+      <Stat title="تاریخ عضویت" value={toLocaleDate(user?.createdAt)}>
+        <RiIcon.RiCalendarCheckLine size="29" className="text-orange-400" />
+      </Stat>
+      <Stat title="درآمد زایی" desc="تومان" value={total}>
+        <RiIcon.RiWallet3Line size="27" className="text-cyan-500" />
+      </Stat>
+      <Stat title="آدرس" value="ثبت نشده">
+        <RiIcon.RiMapLine size="27" className="text-red-500" />
+      </Stat>
+    </Stats>
+  );
+};
+
+export default TopUserDetail;
--- src/pages/admin/UserDetail.tsx
+++ src/pages/admin/UserDetail.tsx
@@ -1,13 +1,20 @@
 import { useParams } from "react-router-dom";
 import { useGetDetailUser } from "../../hooks/admin/useGetAdminData";
+import TopUserDetail from "../../features/admin/userDetail/TopUserDetail";
+import Loading from "../../ui/Loading";
 
 const UserDetail = () => {
-  const {phone} = useParams();
-  const {data,isLoading} = useGetDetailUser(String(phone))
-  
-  console.log(data)
+  const { phone } = useParams();
+  const { isLoading } = useGetDetailUser(String(phone));
 
-  return <div>userDetail</div>;
+  if(isLoading) return <Loading/>
+
+  return (
+    <>
+      <TopUserDetail />
+      <hr className="dark:border-slate-700 my-2 border-slate-200" />
+    </>
+  );
 };
 
 export default UserDetail;
